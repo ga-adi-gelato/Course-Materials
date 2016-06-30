fb4c8acac732c16f815e16df14278ee1d275040b
Alan J Caceres <alan.caceres@generalassemb.ly>
2016-06-30 04:57:15 -0400

    Uploading notes and starter code for Views 101 lesson

---
+++ lessons/android/views-101-lesson/starter-code/app/src/main/java/ga/demo/views101/MainActivity.java
@@ -0,0 +1,15 @@
+package ga.demo.views101;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
