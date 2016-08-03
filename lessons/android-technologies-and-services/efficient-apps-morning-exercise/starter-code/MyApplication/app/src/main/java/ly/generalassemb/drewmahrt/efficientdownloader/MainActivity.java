e79f885dc1c5070352dc38452961805bc673f426
charlesdrews <charlesdrews@gmail.com>
2016-08-03 15:23:59 -0400

    Add efficient apps morning exercise

---
+++ lessons/android-technologies-and-services/efficient-apps-morning-exercise/starter-code/MyApplication/app/src/main/java/ly/generalassemb/drewmahrt/efficientdownloader/MainActivity.java
@@ -0,0 +1,78 @@
+package ly.generalassemb.drewmahrt.efficientdownloader;
+
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private int mCount;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text);
+        mCount = 0;
+
+        final Button button = (Button)findViewById(R.id.counter_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCount++;
+                button.setText("Count: " + mCount);
+            }
+        });
+
+        downloadImage("http://www.nasa.gov/sites/default/files/thumbnails/image/hs-2015-02-a-hires_jpg.jpg");
+
+        mTextView.setTextColor(Color.GREEN);
+        mTextView.setText("All done!");
+    }
+
+
+
+
+    //Don't modify this method
+    private void downloadImage (String url){
+        URL currentUrl = null;
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        BufferedInputStream in = null;
+        try {
+            currentUrl = new URL(url);
+            in = new BufferedInputStream(currentUrl.openStream());
+            byte[] byteChunk = new byte[4096];
+            int n;
+
+            while ( (n = in.read(byteChunk)) > 0 ) {
+                baos.write(byteChunk, 0, n);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
