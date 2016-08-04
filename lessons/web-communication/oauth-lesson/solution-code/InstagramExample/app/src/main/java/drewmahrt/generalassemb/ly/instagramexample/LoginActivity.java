aab73808546e127d49ad0ffedba3b3bfdc7a5f11
Alan J Caceres <alan.caceres@generalassemb.ly>
2016-08-04 16:38:20 -0400

    Uploading correct solution code folder

--- lessons/web-communication/oauth-lesson/solution-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/LoginActivity.java
+++ lessons/web-communication/oauth-lesson/solution-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/LoginActivity.java
@@ -1,15 +1,35 @@
 package drewmahrt.generalassemb.ly.instagramexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
     WebView mWebView;
 
-    private final String YOUR_AUTHORIZATION_URL = ""; //ENTER YOUR AUTHORIZATION URL HERE
+    private final String YOUR_AUTHORIZATION_URL =
+            new StringBuilder("https://api.instagram.com/oauth/authorize/?")
+            .append("client_id=").append(InstagramAppData.CLIENT_ID)
+            .append("&redirect_uri=" ).append(InstagramAppData.CALLBACK_URL).append("&response_type=code")
+            .toString(); //ENTER YOUR AUTHORIZATION URL HERE
+
     private static final String TAG = "LoginActivity";
 
     @Override
@@ -37,7 +57,7 @@ public class LoginActivity extends AppCompatActivity {
                     int index = url.indexOf("=");
                     Log.i(TAG, url.substring(index+1));
 
-                    //STRIPPING AWAY THE URL AND ONLY KEEPING THE CODE
+                    //STRIPPING AWAY THE URL AND ONLY KEEPING THE "CODE"
                     String code = url.substring(index+1);
                     getAccessToken(code);
                     return true;
@@ -52,12 +72,72 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    /** Pseudocode
+     *
+     * Now we have the auth code
+     * Make a request to the server that includes the code, client id, client secret, redirect uri
+     * We get an access token as a JSON Object
+     * Parse the JSON Object to get the access token
+     * Make our api calls
+     *
+     *
+     */
+
+
     /**
      * WE WILL WORK ON THIS TOGETHER
      * @param code THE CODE WE RETRIEVED FROM THE REDIRECT URL
      */
     private void getAccessToken(String code){
         //WE'LL WORK ON THIS TOGETHER
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("client_id", InstagramAppData.CLIENT_ID)
+                .add("client_secret", InstagramAppData.CLIENT_SECRET)
+                .add("redirect_uri", InstagramAppData.CALLBACK_URL)
+                .add("code", code)
+                .add("grant_type", "authorization_code")
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://api.instagram.com/oauth/access_token")
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: request failed", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Unexpected code " + response);
+                }
+
+                String responseString = response.body().string();
+                Log.i(TAG, "onResponse: " + responseString);
+
+                try {
+                    JSONObject result = new JSONObject(responseString);
+                    String accessToken = result.getString("access_token");
+
+                    Log.i(TAG, "onResponse: access token - " + accessToken);
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("access_token", accessToken);
+                    startActivity(intent);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
     }
 
 
--- lessons/web-communication/oauth-lesson/solution-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/MainActivity.java
+++ lessons/web-communication/oauth-lesson/solution-code/InstagramExample/app/src/main/java/drewmahrt/generalassemb/ly/instagramexample/MainActivity.java
@@ -2,7 +2,22 @@ package drewmahrt.generalassemb.ly.instagramexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private static String mAccessToken;
@@ -15,6 +30,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mImage = (ImageView)findViewById(R.id.image);
+
+        mAccessToken = getIntent().getStringExtra("access_token");
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.instagram.com/v1/users/self/media/recent/?access_token="
+                        + mAccessToken).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: Something failed", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Derp");
+                }
+
+                String responseString = response.body().string();
+
+                try {
+                    JSONObject result = new JSONObject(responseString);
+                    JSONArray dataArray = result.getJSONArray("data");
+                    JSONObject dataObject = dataArray.getJSONObject(0);
+                    JSONObject imageObject = dataObject.getJSONObject("images");
+                    JSONObject standardResImageObect =
+                            imageObject.getJSONObject("standard_resolution");
+                    final String imageUrl = standardResImageObect.getString("url");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Picasso.with(MainActivity.this)
+                                    .load(imageUrl)
+                                    .into(mImage);
+                        }
+                    });
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
     }
 }
