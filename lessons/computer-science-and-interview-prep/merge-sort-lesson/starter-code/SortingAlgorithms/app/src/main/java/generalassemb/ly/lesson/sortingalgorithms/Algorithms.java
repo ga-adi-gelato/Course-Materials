e730eaebe90ad69013b69c15badeca007eb4018b
Alan J Caceres <alan.caceres@generalassemb.ly>
2016-08-29 09:55:02 -0400

    Uploading lesson notes for Merge Sort Lesson

---
+++ lessons/computer-science-and-interview-prep/merge-sort-lesson/starter-code/SortingAlgorithms/app/src/main/java/generalassemb/ly/lesson/sortingalgorithms/Algorithms.java
@@ -0,0 +1,24 @@
+package generalassemb.ly.lesson.sortingalgorithms;
+
+/**
+ * Created by alanjcaceres on 8/29/16.
+ */
+public class Algorithms {
+
+    public int[] mergeSort(int[] array){
+        /*
+            Code in your merge sort algorithm
+            It should perform the algorithm "in-place".
+            Meaning, DO NOT MAKE A COPY OF THE ARGUMENT
+         */
+        return array;
+    }
+
+    private int[] merge(int[] firstArray, int[] secondArray){
+        int[] result = null;
+
+        return result;
+    }
+
+
+}
